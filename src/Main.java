caa53f881aab4d6e8de7438d42cdc6ab34dab5cb
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-10-01 08:44:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*Scanner sc = new Scanner(System.in);
+
+        int number = (int)(Math.random()*10);
+        int number2 = (int)(Math.random()*10);
+
+        System.out.println("What is " + number + "+" + number2);
+
+        int answer = sc.nextInt();
+
+        while (number + number2 != answer){
+            System.out.println("Wrong try again faggot...");
+         sc.nextInt();
+        }
+
+        System.out.println("You got it");*/
+
+
+        System.out.println("\u0031");
+        System.out.println("\u0041");
+        System.out.println("\u0042");
+        System.out.println("\u0062");
+        System.out.println("40 = " + (char)40);
+        System.out.println("59 = " + (char)59);
+        System.out.println("79 = " + (char)79);
+        System.out.println("85 = " + (char)85);
+        System.out.println("90 = " + (char)90);
+        System.out.println("40 = " + '\u0040');
+        System.out.println("5A = " + '\u005A');
+        System.out.println("71 = " + '\u0071');
+        System.out.println("72 = " + '\u0072');
+        System.out.println("7A = " + '\u007a');
+        System.out.println("40 = " + '\u3fFa');
+        System.out.println("\\ and \" " );
+        int i = '1';
+        System.out.println(i);
+        int j = '1' + '2' * ('4' - '3') + 'b' / 'a';
+        System.out.println(j);
+        int k = 'a';
+        char c = 90;
+        System.out.println(k+" " + (char)90);//can just call directly if needed else i could call C to get the Z.
+
+        int i1 = (int)c;
+        System.out.println(i1);
+        float f = (float) 1000.34;
+
+        System.out.println(f);
+        double f1 = 1000.34;
+        System.out.println(f1);
+
+        int o = 97;
+        char p = (char)o;
+        System.out.println(p);
+
+        char x = 'a';
+        char y = 'c';
+        System.out.println(++x);
+        System.out.println(y++);
+        System.out.println(x - y);
+
+        System.out.println((int)'a');
+        System.out.println((int)'z');
+
+
+        }
+
+    }
+
